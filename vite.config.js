266806452a159a3b703763df62801265a2abef3a
Vanco-spec <vincentmurithi066@gmail.com>
2025-11-13 20:22:04 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
       input: {
         main: './index.html',  // Main entry point for your website
         events: './events.html',
-        publication: './about-magazine.html',
+        publication: './resource.html',
         submission: './submission-magazine.html',
         issue: './issue-magazine.html',
         team: './team-magazine.html',
